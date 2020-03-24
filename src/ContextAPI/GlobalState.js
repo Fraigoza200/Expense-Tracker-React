b3960aade900ac3d4f652baa02b0b999e247fdf2
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-03-24 10:58:19 -0700

    Add Transaction

--- src/ContextAPI/GlobalState.js
+++ src/ContextAPI/GlobalState.js
@@ -2,14 +2,7 @@ import React, {createContext, useReducer } from 'react'
 import AppReducer from './AppReducer'
 
 // Initial State 
-const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
-}
+const initialState = {transactions:[]}
 
 // create context 
 
@@ -27,7 +20,15 @@ export const GlobalProvider = ({ children }) => {
         })
     }
 
-    return (<GlobalContext.Provider value = {{transactions:state.transactions,deleteTransaction}}>
+
+   function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
+    return (<GlobalContext.Provider value = {{transactions:state.transactions,deleteTransaction,addTransaction}}>
         {children}
     </GlobalContext.Provider>)
 }
