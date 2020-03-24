6b14d9d6a89150437c4f679c3d137535990950b1
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-03-24 10:20:44 -0700

    Create Context

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './App.css'
 import { Header } from './components/Header'
 import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses'
+import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
 
+import { GlobalProvider } from './ContextAPI/GlobalState'
 function App() {
   return (
-    <div>
+    <GlobalProvider>
      <Header />
      <div className='container'>
     <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
      </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
